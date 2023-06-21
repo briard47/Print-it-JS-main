5428038c02343748dfb4ddee06e24dc4d5ed163b
Cendrine Briard <cendrine_briard@protonmail.com>
2023-06-21 12:58:01 +0200

    first commit

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+///Slides///
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+const bannerImage = document.getElementById("bannerImage");
+const bannerText = banner.querySelector("p");
+let currentSlide = 0; 
+
+///Flèche Gauche///
+const leftArrow=document.querySelector('.arrow_left')
+leftArrow.addEventListener('click',showPreviousSlide)
+function showPreviousSlide(){
+	currentSlide = (currentSlide - 1 );
+	if (currentSlide < 0) currentSlide = slides.length - 1;//loop//
+	bannerImage.src = "./assets/images/slideshow/" + slides[currentSlide].image; 
+	bannerText.innerHTML = slides[currentSlide].tagLine;
+	updateActiveDot();	
+}
+
+///Flèche Droite///
+const rightArrow=document.querySelector('.arrow_right')
+rightArrow.addEventListener('click', showNextSlide)
+function showNextSlide(){
+	currentSlide = (currentSlide + 1) ; 
+	if (currentSlide >= slides.length) currentSlide = 0;//loop//
+	bannerImage.src = "./assets/images/slideshow/" + slides[currentSlide].image;
+	bannerText.innerHTML = slides[currentSlide].tagLine;
+	updateActiveDot();
+}
+///Dots///
+const dots = document.querySelectorAll(".dot");
+updateActiveDot() 
+function updateActiveDot() {
+	dots.forEach((dot, index) => {
+		dot.classList.remove("dot_selected");
+		if (index === currentSlide) {
+			dot.classList.add("dot_selected");
+		}
+	});
+}
